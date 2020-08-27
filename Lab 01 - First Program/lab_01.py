3e7e23211a9c3464ab3c43be7806fa63f1de19ee
Tristan Bman <tristan.bocook@my.simpson.edu>
2020-08-27 16:39:00 -0500

    Work on lab 01

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-print("Hello World")
+print("Bowman says to woods,\"I spot Charlie.\"")
+print("So Bowman and Woods sneak up to charlie.")
+print("Bowman whispers,\"psst Charlie over here.\"")
+print("Charlie bewildered,trips on himself and falls to the ground.")
